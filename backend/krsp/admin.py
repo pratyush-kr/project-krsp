60050a8d6aaceaf36921e69860cf7b2e7faf4dc7
pratyush-kr <pratyush151198@gmail.com>
2023-04-30 10:32:30 +0530

    New Minor changes

--- backend/krsp/admin.py
+++ backend/krsp/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your tests_models here.
 from krsp.models import *
 
 admin.site.register(Appointment)
+admin.site.register(Chat)
+admin.site.register(ChatRoom)
 admin.site.register(Doctor)
+admin.site.register(InfoPage)
+admin.site.register(MedicalHistory)
 admin.site.register(Order)
+admin.site.register(OTP)
 admin.site.register(Patient)
+admin.site.register(Ratings)
+admin.site.register(Scheme)
 admin.site.register(User)
